6b02bb24ddde3e248747a580040a66e3d9db6cab
Zainbkhalid <137535381+Zainbkhalid@users.noreply.github.com>
2023-06-24 10:58:35 +0300

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Covenant records',
-    'category': 'covenant Management',
-    'summary': '',
-    'description': "",
-    'version': '1.0',
-    'depends': ['base'],
-    'data': ['view\covenant_record_views.xml',
-             'security\ir.model.access.csv',
-    ],
-    'installable': True,
-    'application': True,
-    'license': 'LGPL-3',
+  'name': 'Car Store',
+  'version': '1.0',
+  'category': 'Accounting/Accounting',
+  'author': 'Salsaabel Techon Solutions',
+  'website': "",
+  'company': 'Salsaabel Techon Solutions',
+  'maintainer': 'Salsaabel Techon Solutions',
+  'summary': 'Record Car Details',
+  "description": "",
+  'depends': ['base'],
+   'data': [
+      'security/ir.model.access.csv',
+      'views/car_store_views.xml',
+  ],
+  'installable':True,
+  'application':True,
+  'license':'LGPL-3',
+ 
 }
